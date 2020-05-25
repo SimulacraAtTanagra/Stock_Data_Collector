d62f82d4b93a498bc50bd27a8e624582450399f2
SimulacraAtTanagra <shaneayers1@gmail.com>
2020-05-24 20:30:34 -0400

    updated query

--- query.sql
+++ query.sql
@@ -1,4 +1,10 @@
-select name, max(high) as Max_High, extract(hour from cast(ts as timestamp)) as Hour_of_Day
+select a.name, a.high, a.ts, a.hour
+FROM ( select name, high, ts, extract(hour from cast(ts as timestamp)) as "hour" from stock_data_delivery) a, (select name, max(high) as "maxhigh", extract(hour from cast(ts as timestamp)) as "hour"
 from stock_data_delivery
-group by (name, extract(hour from cast(ts as timestamp)))
-order by name, Hour_of_Day
+group by (name, extract(hour from cast(ts as timestamp))))b
+where (a.high = b.maxhigh and a.name = b.name and a.hour = b.hour)
+order by a.name,a.hour
+
+
+
+
